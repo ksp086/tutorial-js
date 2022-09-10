9165eb181aa63d9b6d048d417d4996801aad18dc
ksp086 <dev.kashyap.patel.086@gmail.com>
2022-09-10 21:56:04 +0530

    Chapter 2 1. defining a variable 2. using a variable 3. types of variables 4. arrays and objects

---
+++ chapter2/main.js
@@ -0,0 +1,60 @@
+// 1
+// defining a variable
+// var myVar = "This is a variable";
+// console.log(myVar);
+// string variable
+
+// 2
+// using a variable
+/*
+var number1 = 5;
+number1 = 3;
+console.log(number1);
+var number2 = number1 * 10;
+console.log(number2);
+var number3 = number2 / number1;
+console.log(number3);
+*/
+
+// 3
+// types of variables
+/*
+var myInt = 21; // 32 bit
+var myFloat = 5.5; // 32 bit 
+console.log("Int:", myInt, "Float:", myFloat);
+var myLong = 9310141419482 // 64 bit
+var myDouble = 9310141419482.98 // 64 bit
+console.log("Long:", myLong, "Double:", myDouble);
+var bool1 = true;
+var bool2 = false;
+console.log("True:", bool1, "False:", bool2);
+var myNotANumber = NaN;
+var NaNExample = 0/0;
+console.log("NaN:", myNotANumber, "0/0:", NaNExample);
+var notDefined; // undefined
+console.log("UnDefined:", notDefined);
+var myNull = null;
+console.log("Null:", myNull);
+*/
+
+// 4
+// arrays and objects
+/*
+var favoriteFruits = ["Apple", "Banana", "Orange", "Strawberry"];
+var primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31];
+var randomVar = [2, "String", undefined, null, NaN, true, 2.1];
+console.log(favoriteFruits[0]);
+var myObject = {};
+var john = {
+    firstName: "John",
+    lastName: "Doe", 
+    fullName: "John Doe"
+}
+var billy = {
+    firstName: "Billy",
+    lastName: undefined, 
+    fullName: "Billy"
+}
+console.log(john.fullName);
+console.log(billy.lastName);
+*/
