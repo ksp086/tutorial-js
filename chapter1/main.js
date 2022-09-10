970b5f58412fe07007b4c1009688e3859afd450f
ksp086 <dev.kashyap.patel.086@gmail.com>
2022-09-10 13:48:01 +0530

    Chapter 1 1. console log 2. dom api 3. adding element 4. alert 5. prompt 6. confirm

---
+++ chapter1/main.js
@@ -0,0 +1,68 @@
+// log to console
+// console.log("Hello World");
+
+// logging variables
+/*
+var thisVar = 'first value';
+var thatVar = 'second value';
+console.log("thisVar:", thisVar, "and thatVar:", thatVar);
+*/
+
+// place holders
+/*
+var greet = "Hello", who = "World";
+console.log("%s, %s", greet, who);
+*/
+
+// logging objects
+/*
+console.log({
+    'Name': 'abc',
+    'Email': 'abc@gmail.com',
+    'Groups': {}
+});
+*/
+
+// logging html elements
+// 1.html
+// 1
+
+// dom api
+// document object model
+// oo representation of structured docs like xml and html
+// 1.html
+// 2
+
+// adding element
+// 1.html
+// 3
+
+// window alert
+// visual alert box on screen
+// no other js code will execute until the alert is dismissed
+// 1.html
+// 4
+
+// 5
+// window prompt
+// ask user for input
+// prompt(text, [default]);
+/*
+var age = prompt("How old are u?");
+console.log(age);
+*/
+// on clicking ok input value is returned
+// return value is always a string
+// cancel returns null
+// when displayed, user is prevented from accessing other parts of the page
+
+// 6
+// window confirm
+// ok or cancel
+/*
+var itemId = "paragraph";
+var message = "Are u sure u want to delete this?";
+if(window.confirm(message)) {
+    deleteItem(itemId);
+}
+*/
